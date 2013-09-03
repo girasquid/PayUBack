dcf79aee734e4ed481ab499a1588e74f9ffb0167
Jeremy Smereka <ekoedmedia@gmail.com>
2013-09-03 01:12:33 -0600

    Added parent::__construct to User Model

--- src/Ekoed/UserBundle/Entity/User.php
+++ src/Ekoed/UserBundle/Entity/User.php
@@ -81,6 +81,7 @@ class User extends BaseUser
      */
     public function __construct()
     {
+        parent::__construct();
         $this->userGroups = new \Doctrine\Common\Collections\ArrayCollection();
     }
     
