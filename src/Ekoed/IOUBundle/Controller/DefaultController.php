e238e8d9dbe8df453696c1f8daf00e683538f628
Jeremy Smereka <ekoedmedia@gmail.com>
2013-09-08 01:05:37 -0600

    Changes to Make AJAX Add Work - Removed changes from routing.yml - Removed newly created controller + Moved old function from removed controller to DefaultController.php in IOUBundle + Added Annotations to DefaultController.php to maintain consistency across bundles - Removed installed second Symfony2? Unsure where this came from

--- src/Ekoed/IOUBundle/Controller/DefaultController.php
+++ src/Ekoed/IOUBundle/Controller/DefaultController.php
@@ -4,17 +4,33 @@ namespace Ekoed\IOUBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
+use Ekoed\IOUBundle\Entity\IOU as IOU;
 
 class DefaultController extends Controller
 {
  
- public function CreateIOUAction(){
-
- 	        //prepare the response, e.g.
-        $response = array("Status" => 100, "Data" => $output);
-        //you can return result as JSON
-        return new Response(json_encode($response)); 
- }   
+ 	/**
+     * @Route("/iou/create")
+     * @Method({"POST"})
+     * @Template()
+     */
+	public function createIOUAction(){
+			$em = $this->getDoctrine()->getManager();
+	 		$IOU = new IOU();
+	 		$IOU->setDateAdded(new \DateTime());
+	 		$IOU->setType("liter");
+	 		$IOU->setAmount("100");
+	 		$IOU->setDescription("wat");
+	 		$IOU->setAutocalcOverride(1);
+	 		$em->persist($IOU);
+	        $em->flush();
+	 	        //prepare the response, e.g.
+	        $response = array("Status" => 100, "Data" => "ADDED");
+	        //you can return result as JSON
+	    	return new Response(json_encode($response)); 
+	}   
 
 }
