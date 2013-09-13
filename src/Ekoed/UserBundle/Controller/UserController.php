6b37ff6b2cd25b73a2e541c4866a015f0082df4f
asalimi <asalimi@ualberta.ca>
2013-09-13 01:26:08 -0600

    worked on the add contact module

--- src/Ekoed/UserBundle/Controller/UserController.php
+++ src/Ekoed/UserBundle/Controller/UserController.php
@@ -4,13 +4,17 @@ namespace Ekoed\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-
+use Symfony\Component\HttpFoundation\Response;
+use Ekoed\UserBundle\Entity\User as User;
 class UserController extends Controller
 {
 
     public function addContactAction()
     {
-        return array('name' => "22123");
+
+    	$response=array("status"=>"success","reason"=>"");
+    	return new Response(json_encode($response)); 
     }
 }
