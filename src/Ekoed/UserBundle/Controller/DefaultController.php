009afb6890db5d9b644c3ba52b2b7e24e6e638f7
ekoedmedia <ekoedmedia@gmail.com>
2013-09-13 13:36:20 -0600

    Changed controller to be in Default Controller

--- src/Ekoed/UserBundle/Controller/DefaultController.php
+++ src/Ekoed/UserBundle/Controller/DefaultController.php
@@ -16,5 +16,57 @@ class DefaultController extends Controller
     {
         return array('name' => "22123");
     }
-    
+
+    /**
+     * @Route("/waasa")
+     * @Template()
+     */
+    public function addContactAction()
+    {
+		//if user exists, make iou, else make user and then make it
+		$request = $this->container->get('request');    
+	 	$em = $this->getDoctrine()->getManager();
+	 	//if email format, look for email, else look for username
+	 	if($request->request->get('format')=="email") {
+			//look for the username
+			$query = $em->createQuery(
+			    'SELECT u
+			    FROM EkoedUserBundle:user u
+			    WHERE u.email = :email '
+			)->setParameter('email', $request->request->get('contact'));
+				
+			$recipient = $query->getResult();
+			//if found, add contact notifier, else ??????
+		 	if(!$recipient) {
+		 		$response = array("Status" => 100, "Data" => "confirm");
+		 		return new Response(json_encode($response)); 
+		 	} else {
+			 	    //prepare the response, 
+			        $response = array("Status" => 100, "Data" => "ADDED");
+			        //you can return result as JSON
+			    	return new Response(json_encode($response)); 
+			}
+	 	} else {
+	 		//look for username, if found send notifier
+			$query = $em->createQuery(
+			    'SELECT u
+			    FROM EkoedUserBundle:user u
+			    WHERE u.username = :username '
+			)->setParameter('username', $request->request->get('contact'));
+			
+			$recipient = $query->getResult();
+		 	if(!$recipient) {
+		 		
+		 		$response = array("Status" => 100, "Data" => "confirm");
+		 		return new Response(json_encode($response)); 
+		 	} else {
+			 	    //prepare the response, 
+			        $response = array("Status" => 100, "Data" => "ADDED");
+			        //you can return result as JSON
+			    	return new Response(json_encode($response)); 
+			}
+
+		}
+	}   
+
 }
